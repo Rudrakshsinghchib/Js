5b7370368291cfc055e32a27a49db34f97bb6ab1
Rudrakshsinghchib <163580051+Rudrakshsinghchib@users.noreply.github.com>
2024-11-27 20:37:24 +0000

    commit variable

---
+++ 01_basics/Variable.js
@@ -0,0 +1,8 @@
+const accountNumber=13244
+var AccountCity="bengal"
+let AccountEmail="rudra@gmail.com"
+// console.log(AccountEmail)
+AccountCity="jammu"
+let account;
+console.log(account)
+// console.table([accountNumber,AccountCity,AccountEmail])
