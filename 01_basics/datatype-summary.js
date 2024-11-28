9c9aaaca81856b276d83ede2fb527d0c8fdf72cb
Rudrakshsinghchib <163580051+Rudrakshsinghchib@users.noreply.github.com>
2024-11-28 16:31:23 +0000

    heap and stack memory

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -30,3 +30,19 @@ const fun=function() {
 console.log(typeof fun)
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
+// ============== Memory Stack(non  primitive-copy) Heap(primitive reference)
+let name="rudra"
+let yourname=name
+yourname="rudi"             // stack stuff
+// console.log(yourname)
+// console.log(name)
+
+
+              // heap
+              let mystory= {
+                name:"vibhu",
+                age:24,
+              }
+              let vi=mystory
+              vi.age=29
+              console.log(mystory.age)
