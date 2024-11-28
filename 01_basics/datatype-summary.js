ff4b77e6915beeda41bf5226ff6a341ff4fbbcd1
Rudrakshsinghchib <163580051+Rudrakshsinghchib@users.noreply.github.com>
2024-11-28 15:20:20 +0000

    datatypes

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -1,1 +1,31 @@
 // Primitive type
+// 7 types:String,Number,boolean,null,undefined,symbol,Bigint
+
+const score=100
+const scoreValue=100.3  //dynamic language
+
+const isLoggedIn=false
+const outsidetemp=null
+let email
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id=== anotherId)
+
+// Reference (Non-primitive)
+//Array,Object,Functions
+
+const heros=["toji","sukuna","dogi"];
+
+let myObj= {
+    name:"rudra",
+    age:20,
+}
+console.log(heros)
+
+const fun=function() {
+    console.log("hi");
+}
+
+console.log(typeof outsidetemp)
+
