91a10420bdec6f9ff0f588afe1535b714871e458
Rudrakshsinghchib <163580051+Rudrakshsinghchib@users.noreply.github.com>
2024-11-28 12:11:26 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score=33
+let convert=String(score)
+// console.log(typeof convert)
+
+let name1="33abc"
+let convert1=Number(name1)
+// console.log(typeof convert1)
+// console.log(convert1)
+
+// let num=NaN
+// let convert2=Boolean(num)
+// console.log(convert2)
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// "33" => 33
+// "33abc"=>NaN
+// true =>1; false =>0
+
+// console.log((3+4)*3%3)
+// let st1="hi"
+// let st2=" rudra"
+// let sr3=st1+st2 
+// console.log(sr3)
+
+let i=0;
+console.log(i++)
+// console.log(++i)
