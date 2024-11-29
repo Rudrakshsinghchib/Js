678a010909f77cb6e5f74328aeae50d53c5f55ed
Rudrakshsinghchib <163580051+Rudrakshsinghchib@users.noreply.github.com>
2024-11-29 13:23:35 +0000

    ni

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,13 @@
+let name=`rudra`
+let yo=`string`
+console.log(`hi i am ${name} and i will improve my ${yo}`)
+console.log(name+yo)
+
+let rudra=new String('hirudra')
+console.log(rudra.indexOf('h'))
+console.log(rudra[1])
+console.log(rudra.substring(0,3))
+// you have to look into the docs for more info its necessary
+
+// rudra.replace(i,h)
+// console.log(rudra)
