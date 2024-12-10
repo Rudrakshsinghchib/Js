e2eafa356bae54f4b553a2dbf139525814508f68
Rudrakshsinghchib <163580051+Rudrakshsinghchib@users.noreply.github.com>
2024-12-10 09:00:54 +0000

    commit

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// Immediately invoked function Expression(IIfe)
+// to protect it from global scope pollution and jo function immediately execute hojaya
+// ()() that's it
+
+(function() {
+    console.log(1)
+})();
+
+( ()=> {
+    console.log("CONNECTEd");
+})()
+
