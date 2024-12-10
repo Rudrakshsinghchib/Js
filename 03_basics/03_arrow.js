59b9e5ff3ed216c2588f9507ecc12b2d41a1d9da
Rudrakshsinghchib <163580051+Rudrakshsinghchib@users.noreply.github.com>
2024-12-10 09:00:29 +0000

    iife and arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -13,6 +13,45 @@ const user= {
 
 }
 
+
+
 // user.welcomeMessage();
-user.name="rudra"
-user.welcomeMessage();
+// user.name="rudra"
+// user.welcomeMessage();
+
+function chai() {
+    let username="rudra"
+    console.log(this.username)
+}
+console.log(chai())
+
+
+//  const chai=function () {       // this sirf object ka andar kaam karta
+//     let username="rudra"
+//     console.log(this.username)
+// }
+// console.log(chai);
+
+// this here - { } in browser console-global variable
+
+
+// const addTwo=(num1,num2) => {
+//     return num2+num1
+// }
+
+const addTwo=(num1,num2) => (num1+num2)
+
+// console.log(addTwo(3,4))
+// () => {} this is arraow function
+
+// implicit fun - one line so no need to return i know
+
+const add=(num4,num3) => (num4+num3)
+// console.log(add(2,3))
+
+const r=() => ({user:"rudra"})
+// console.log(r)
+
+const arr=[3,4,6,8]
+// arr.forEach()
+
